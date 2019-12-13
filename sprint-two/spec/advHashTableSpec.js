036be5f7cf65a8f5a015feebb2d88be86170a760
Andrew Song <44534521+AndrewSong1122@users.noreply.github.com>
2019-12-13 14:35:54 -0800

    Added tests.
    Added tests to check if the hash table maintains key-value associations after resizing

--- sprint-two/spec/advHashTableSpec.js
+++ sprint-two/spec/advHashTableSpec.js
@@ -48,29 +48,83 @@ describe('advHashTable', function() {
   });
 
   // (Advanced! Remove the extra "x" when you want the following tests to run)
-  it ('should double in size when needed', function() {
+  it ('should double in size when needed and maintain key-value associations', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
       hashTable.insert(firstName, lastName);
       expect(hashTable.retrieve(firstName)).to.equal(lastName);
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
     });
     expect(hashTable._limit).to.equal(16);
   });
 
-  it ('should halve in size when needed', function() {
+  it ('should halve in size when needed and maintain key-value associations', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
       hashTable.insert(firstName, lastName);
       expect(hashTable.retrieve(firstName)).to.equal(lastName);
+      expect(hashTable.retrieve(firstName)).to.equal(lastName);
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
     });
     expect(hashTable._limit).to.equal(16);
+    var firstName;
+    var lastName;
+
+    firstName = 'George';
+    lastName = hashTable.retrieve(firstName);
     hashTable.remove('George');
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
+
+    firstName = 'Dr.';
+    lastName = hashTable.retrieve(firstName);
     hashTable.remove('Dr.');
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
+
+    firstName = 'Steven';
+    lastName = hashTable.retrieve(firstName);
     hashTable.remove('Steven');
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
+
+    firstName = 'John';
+    lastName = hashTable.retrieve(firstName);
     hashTable.remove('John');
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
+
+    firstName = 'Mr.';
+    lastName = hashTable.retrieve(firstName);
     hashTable.remove('Mr.');
+      hashTable._storage.each(function(p) {
+          if (p !== undefined) {
+            expect(hashTable.retrieve(p[0])).to.equal(p[1]);
+          }
+        });
+      
     expect(hashTable._limit).to.equal(8);
   });
 });
